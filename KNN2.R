3c78c7f7ea39e8bc223a2ddb10e9d5b33d117d38
cpclark93 <45441399+cpclark93@users.noreply.github.com>
2018-11-29 01:27:18 +0100

    Add files via upload

---
+++ KNN2.R
@@ -0,0 +1,32 @@
+# Libraries.
+library(caret)
+library(pROC)
+library(mlbench)
+
+# Data Classification using Carevaluations.txt
+data <- read.table(file.choose(), header = TRUE)
+str(data)
+
+# Partioning the data.
+set.seed(50)
+ind <- sample(2, nrow(data), replace = TRUE, prob = c(0.70, 0.30))
+training <- data[ind == 1,]
+test <- data[ind == 2,]
+
+# KNN Modeling.
+trControl <- trainControl(method = "repeatedcv",
+                          number = 10,
+                          repeats = 3)
+set.seed(100)
+fit <- train(Evaluation ~.,
+             data = training,
+             method = "knn",
+             tuneLength = 20,
+             trControl = trControl)
+# Model performance.
+fit
+plot(fit)
+varImp(fit)
+set.seed(100)
+pred <- predict(fit, newdata = examtest)
+confusionMatrix(pred, examtest$Evaluation)
